cad412357cdcf7f5c0990cea00a6d8671d88ce0d
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-03-03 17:07:17 +0000

    Complete casting assignment

--- src/main/java/CastingPrimitive.java
+++ src/main/java/CastingPrimitive.java
@@ -16,6 +16,7 @@ public class CastingPrimitive {
      * @return the int form of d.
      */
     public int castToInt(double d){
-        return 0;
+        int a = (int)d;
+        return a;
     }
 }
